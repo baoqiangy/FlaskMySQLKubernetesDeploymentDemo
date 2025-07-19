69a9073c755a4ab6c63c4a8da430ddf80de83b76
Baoqiang Yan <baoqiang.yan@athens.edu>
2025-07-19 14:13:47 -0500

    Changed the yaml extension from yaml to yml

--- teardown.sh
+++ teardown.sh
@@ -1,14 +1,14 @@
 # Delete the Flask service
-kubectl delete -f flask-service.yaml
+kubectl delete -f flask-service.yml
 
 # Delete the Flask deployment
-kubectl delete -f flask-deployment.yaml
+kubectl delete -f flask-deployment.yml
 
 # Delete the MySQL service
-kubectl delete -f mysql-service.yaml
+kubectl delete -f mysql-service.yml
 
 # Delete the MySQL deployment
-kubectl delete -f mysql-deployment.yaml
+kubectl delete -f mysql-deployment.yml
 
 # Delete the persistent volume claim
-kubectl delete -f mysql-pv-claim.yaml
+kubectl delete -f mysql-pv-claim.yml
